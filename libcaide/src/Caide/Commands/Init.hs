e1fc0485f03166b54bdbc404e57325029a003ffb
slycelote <slycelot@yandex.ru>
2014-03-23 15:18:28 +0400

    Create code templates during caide init

--- libcaide/src/Caide/Commands/Init.hs
+++ libcaide/src/Caide/Commands/Init.hs
@@ -2,7 +2,9 @@ module Caide.Commands.Init(
       cmd
 ) where
 
-import Filesystem (createDirectory)
+import Control.Monad (forM_)
+import qualified Data.Text as T
+import Filesystem (createDirectory, writeTextFile)
 
 import qualified Filesystem.Path as F
 import Filesystem.Path.CurrentOS (decodeString, encodeString, (</>))
@@ -21,4 +23,26 @@ initialize :: F.FilePath -> [String] -> IO ()
 initialize curDir _ = do
     let caideDir = curDir </> decodeString ".caide"
     createDirectory False caideDir
+    forM_ defaultTemplates $ \(fileName, fileContents) ->
+        writeTextFile (curDir </> decodeString fileName) (T.pack fileContents)
     putStrLn $ "Initialized caide directory at " ++ encodeString curDir
+
+-- TODO: hardcode text of templates here when finalized
+defaultTemplates :: [(FilePath, String)]
+defaultTemplates = [
+    ("solution_template.cpp",
+
+    "#include <iostream>\n\
+    \\n\
+    \void solve(std::istream& in, std::ostream& out)\n\
+    \{\n\
+    \}\n\
+    \"),
+
+    ("main_template.cpp",
+    ""),
+
+    ("test_template.cpp",
+    "")
+
+    ]
