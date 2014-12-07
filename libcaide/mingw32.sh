76b958333d16f169861ac7c5ce63675b2be6ae17
slycelote <slycelot@yandex.ru>
2014-12-07 03:05:08 +0300

    Update to Haskell Platform 2014.2

--- libcaide/mingw32.sh
+++ libcaide/mingw32.sh
@@ -1,1 +1,1 @@
-PATH="/c/python27:/c/bin/HaskellPlatform/2013.2.0.0/bin:/c/bin/HaskellPlatform/2013.2.0.0/lib/extralibs/bin:/c/bin/HaskellPlatform/2013.2.0.0/mingw/bin:$PATH"
+PATH="/c/python27:/c/bin/HaskellPlatform/2014.2.0.0/bin:/c/bin/HaskellPlatform/2014.2.0.0/lib/extralibs/bin:/c/bin/HaskellPlatform/2014.2.0.0/mingw/bin:$PATH"
