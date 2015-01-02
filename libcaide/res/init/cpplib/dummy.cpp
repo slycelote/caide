d1374ace62a60a1b56d321b0acc2a66a02432e0b
slycelote <slycelot@yandex.ru>
2015-01-02 14:53:29 +0300

    Update templates

--- libcaide/res/init/cpplib/dummy.cpp
+++ libcaide/res/init/cpplib/dummy.cpp
@@ -1,5 +1,19 @@
+void dummy_caide_function () {
 /*
-dummy.cpp: force creation of static library.
-Feel free to remove this file when you added at least one 'real' cpp file.
+dummy.cpp: force creation of static library in VsCaide plugin.
+Feel free to remove this file when you add at least one 'real' cpp file.
+
+Also, note how documentation for the function is *inside* its definition.
+This is a workaround, because otherwise the comment would be included
+into the final submission. In general, avoid top level comments in your library.
+
+This style works too:
+void function_definition()
+// comment
+{
+  function_implementation();
+}
 */
-void dummy_caide_function () {}
+
+}
+
