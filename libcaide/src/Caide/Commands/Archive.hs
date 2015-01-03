00e060b30bba80dff8710667b83ac662c62bf2ed
slycelote <slycelot@yandex.ru>
2015-01-03 19:25:03 +0300

    Switch to another problem after archiving the active one

--- libcaide/src/Caide/Commands/Archive.hs
+++ libcaide/src/Caide/Commands/Archive.hs
@@ -2,12 +2,13 @@ module Caide.Commands.Archive (
       cmd
 ) where
 
+import Prelude hiding (FilePath)
 import Control.Applicative ((<$>))
-import Control.Monad (unless, when, forM_)
+import Control.Monad (unless, when, forM_, forM)
 import Control.Monad.State (liftIO)
 import Data.Time (getZonedTime, formatTime)
-import Filesystem (isDirectory, createTree, removeTree)
-import Filesystem.Path.CurrentOS ((</>), decodeString, encodeString)
+import Filesystem (isDirectory, createTree, removeTree, listDirectory, isDirectory)
+import Filesystem.Path.CurrentOS ((</>), decodeString, encodeString, basename, FilePath)
 import System.Locale (defaultTimeLocale)
 
 import Caide.Configuration (getActiveProblem, setActiveProblem)
@@ -29,20 +30,36 @@ archive [probId] = do
         problemStateDir = problemDir </> decodeString ".caideproblem"
     problemExists <- liftIO $ isDirectory problemStateDir
     unless problemExists $ throw $ "Problem " ++ probId ++ " doesn't exist"
+
+    -- Prepare archive directory
     now <- liftIO getZonedTime
     let formattedDate = formatTime defaultTimeLocale "%F" now
         archiveDir = root </> decodeString "caide_archive" </> decodeString formattedDate </> decodeString probId
     archiveDirExists <- liftIO $ isDirectory archiveDir
     when archiveDirExists $ throw $ "Archive directory for this problem already exists: " ++ encodeString archiveDir
+
     liftIO $ do
         createTree archiveDir
+        -- Copy necessary files
         files <- fst <$> listDir problemDir
         forM_ files $ \file -> copyFileToDir file archiveDir
         copyTreeToDir problemStateDir archiveDir
+        -- Remove problem directory
         removeTree problemDir
+
+    -- Change active problem, if necessary
     activeProblem <- getActiveProblem
-    when (activeProblem == probId) $ setActiveProblem ""
+    when (activeProblem == probId) $ do
+        allProblems <- liftIO $ caideProblems root
+        let newActiveProblem = if null allProblems then "" else head allProblems
+        setActiveProblem newActiveProblem
 
 
 archive _ = throw $ "Usage: " ++ usage cmd
+
+caideProblems :: FilePath -> IO [ProblemID]
+caideProblems rootDir = do
+    dirs <- listDirectory rootDir
+    isCaideProblem <- forM dirs $ \dir -> isDirectory (dir </> decodeString ".caideproblem")
+    return [encodeString (basename dir) | (dir, True) <- zip dirs isCaideProblem]
 
