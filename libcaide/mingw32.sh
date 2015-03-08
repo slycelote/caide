bd04929db692f9553289ec24a7b89a053452976b
slycelote <slycelot@yandex.ru>
2015-03-08 21:58:49 +0300

    Fix test in Windows

--- libcaide/mingw32.sh
+++ libcaide/mingw32.sh
@@ -1,3 +1,3 @@
 HPPATH=/c/bin/HaskellPlatform/with-new-mingw
 PATH="/c/bin/cabal/.cabal-sandbox/bin:/c/python27:$HPPATH/bin:$HPPATH/lib/extralibs/bin:$HPPATH/mingw/bin:/c/bin/mingw/bin:$PATH"
-
+CSC=/c/Windows/Microsoft.NET/Framework/v4.0.30319/csc.exe
