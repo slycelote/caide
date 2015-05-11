6868a42e92749b98d34fc2901274066e57608188
slycelote <slycelot@yandex.ru>
2015-05-11 14:43:53 +0300

    Fix EOF detection in TC comparator

--- libcaide/src/Caide/TestCases/TopcoderDeserializer.hs
+++ libcaide/src/Caide/TestCases/TopcoderDeserializer.hs
@@ -11,7 +11,7 @@ module Caide.TestCases.TopcoderDeserializer(
 import Control.Applicative (Applicative)
 import Control.Monad (unless, when)
 import Control.Monad.Except (MonadError, Except, runExcept, throwError)
-import Control.Monad.State (StateT, MonadState, get, put, modify', runStateT)
+import Control.Monad.State (StateT, MonadState, get, put, runStateT)
 import Data.Char (isSpace)
 import Data.Maybe (isJust)
 import Data.Text (Text)
@@ -43,14 +43,15 @@ peek = do
     return . T.head $ text
 
 advance :: Int -> TopcoderParser ()
-advance n = modify' (T.drop n)
+advance n = do
+    text <- get
+    when (T.length text < n) $ throwError "Unexpected end of file"
+    put $ T.drop n text
 
 scanUntil :: (Char -> Bool) -> TopcoderParser Text
 scanUntil charPred = do
     text <- get
     let (skipped, rest) = T.break charPred text
-    when (T.null rest) $
-        throwError $ T.append "Parse error: required character not found at the following position: " skipped
     put rest
     return skipped
 
@@ -66,7 +67,7 @@ isTokenSeparator :: Char -> Bool
 isTokenSeparator c = isSpace c || isJust (T.find (==c) "{}\",")
 
 readToken :: TopcoderParser Text
-readToken = scanUntil (not . isSpace) >> scanUntil isTokenSeparator
+readToken = scanUntil (not . isSpace) >> peek >> scanUntil isTokenSeparator
 
 readQuotedString :: TopcoderParser Text
 readQuotedString = do
