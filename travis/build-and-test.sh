b7b56028bbe8083c96706ae5febc3c8ad82f747e
slycelote <slycelot@yandex.ru>
2016-02-07 11:01:25 -0800

    Fix syntax in travis script

--- travis/build-and-test.sh
+++ travis/build-and-test.sh
@@ -4,15 +4,12 @@ set -ev
 export CXX=g++-4.9
 export CC=gcc-4.9
 
-
 git submodule update --init --recursive
 cd libcaide
-
 cabal install --only-dependencies
 
 cabal configure
-
-cabal build --ghc-options="-pgml $(CXX)"
+cabal build --ghc-options="-pgml $CXX"
 
 tests/run-tests.sh
 
