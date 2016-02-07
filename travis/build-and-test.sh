a3f398e3f2b8ca700325382f5f8496f2e34a90fd
slycelote <slycelot@yandex.ru>
2016-02-07 10:23:41 -0800

    Setup correct linker in travis

---
+++ travis/build-and-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ev
+
+export CXX=g++-4.9
+export CC=gcc-4.9
+
+
+git submodule update --init --recursive
+cd libcaide
+
+cabal install --only-dependencies
+
+cabal configure
+
+cabal build --ghc-options="-pgml $(CXX)"
+
+tests/run-tests.sh
+
