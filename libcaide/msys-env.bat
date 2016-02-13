794aa95493a767fd05a5df0cfdcb04604f3a5dc2
slycelote <slycelot@yandex.ru>
2016-02-13 15:28:45 -0800

    Update build instructions for Windows

---
+++ libcaide/msys-env.bat
@@ -0,0 +1,5 @@
+rem This script sets up development enviroment in Windows.
+rem Replace the paths according to your system's configuration.
+set HPPATH=c:\bin\HaskellPlatform\7.10.3
+set PATH=c:\python27;C:\bin\msys-w64\bin\;c:\bin\mingw-w64\mingw32\bin;%HPPATH%\bin;%HPPATH%\lib\extralibs\bin;C:\bin\CMake\bin;%PATH%
+set CSC=c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
---
+++ libcaide/msys-env.sh
@@ -0,0 +1,5 @@
+# This script sets up development enviroment in Windows for MSYS shell.
+# Replace the paths according to your system's configuration.
+HPPATH=/c/bin/HaskellPlatform/7.10.3
+export PATH="/c/python27:$PATH:$HPPATH/bin:$HPPATH/lib/extralibs/bin"
+export CSC=/c/Windows/Microsoft.NET/Framework/v4.0.30319/csc.exe
