6319c434b9d414b57ab602edfd601bd3c21309a6
slycelote <slycelot@yandex.ru>
2017-05-21 16:58:43 -0700

    Stub for Firefox server

--- libcaide/src/Main.hs
+++ libcaide/src/Main.hs
@@ -1,13 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Maybe (isJust, isNothing, fromMaybe)
-
 import Filesystem (getWorkingDirectory, isDirectory)
 import qualified Filesystem.Path as F
 import Filesystem.Path.CurrentOS (encodeString, parent, (</>))
 import System.Environment (getArgs)
 import System.Exit (exitWith, ExitCode(ExitFailure))
+import System.IO (hIsTerminalDevice, stdin)
 
 import Caide.Commands (runMain)
 
@@ -21,26 +20,49 @@ findRootCaideDir curDir = do
     then return $ Just curDir
     else findRootCaideDir $ parent curDir
 
+data RunningMode
+    = ExistingDir F.FilePath
+    | NewDir F.FilePath
+    | FirefoxServer
+    deriving (Eq, Show)
+
+findRunningMode :: Bool -> IO RunningMode
+findRunningMode False = do
+    isTerminal <- hIsTerminalDevice stdin
+    if isTerminal
+        then findRunningMode True
+        else return FirefoxServer
+
+
+findRunningMode True = do
+    workDir <- getWorkingDirectory
+    caideDir <- findRootCaideDir workDir
+    return $ case caideDir of
+        Nothing  -> NewDir workDir
+        Just dir -> ExistingDir dir
+
 
 main :: IO ()
 main = do
     args <- getArgs
     let (cmd:_) = args
         mainAction = runMain args
-    case mainAction of
-        Left ioAction -> ioAction
-        Right caideCmd -> do
-            workDir  <- getWorkingDirectory
-            caideDir <- findRootCaideDir workDir
-            case () of
-              _ | cmd /= "init" && isNothing caideDir -> do
-                    putStrLn $ encodeString workDir ++ " is not a valid caide directory"
+    mode <- findRunningMode (null args)
+    case (mode, mainAction) of
+        (FirefoxServer, _) -> undefined
+        (_, Left ioAction) -> ioAction
+        (NewDir dir, Right caideCmd) ->
+            if cmd == "init"
+                then caideCmd dir
+                else do
+                    putStrLn $ encodeString dir ++ " is not a valid caide directory"
                     halt
-                | cmd == "init" && isJust caideDir    -> do
-                    putStrLn "Caide directory already initialized"
+        (ExistingDir dir, Right caideCmd) ->
+            if cmd == "init"
+                then do
+                    putStrLn "Caide directory is already initialized"
                     halt
-                -- special case for init command: pass working directory as the first parameter
-                | otherwise -> caideCmd $ fromMaybe workDir caideDir
+                else caideCmd dir
 
 halt :: IO ()
 halt = exitWith $ ExitFailure 0xCA1DE
