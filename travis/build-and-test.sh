2a6706f500c61dcdd8fe7601f2740ceeaf58f9c6
slycelote <slycelot@yandex.ru>
2017-05-28 09:22:24 -0700

    Add time logging in travis

--- travis/build-and-test.sh
+++ travis/build-and-test.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -ev
 
+date
+
 export CXX=g++-4.9
 export CC=gcc-4.9
 
 git submodule update --init --recursive
+date
+
 cd libcaide
 cabal install --only-dependencies
+date
 
 cabal configure
 cabal build --ghc-options="-pgml $CXX"
+date
 
 export MONO=mono
 tests/run-tests.sh
+date
 
