7c10bbcbd1096bc9fb7767963b19a80d3aa544cc
slycelote <slycelot@yandex.ru>
2018-07-29 16:56:35 -0700

    Fix test

--- libcaide/tests/simplecpp/after-make/prob/submission.cpp
+++ libcaide/tests/simplecpp/after-make/prob/submission.cpp
@@ -33,7 +33,7 @@ void solve(std::istream& in, std::ostream& out)
 void solve(std::istream& in, std::ostream& out);
 int main() {
     using namespace std;
-    ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(false);
     cin.tie(0);
 
 #ifdef CAIDE_STDIN
