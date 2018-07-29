2d502698535858e7182b32c485f1caad350f7891
slycelote <slycelot@yandex.ru>
2018-07-29 00:07:09 -0700

    Fix Windows build

---
+++ libcaide/setup-env.bat
@@ -0,0 +1,18 @@
+@echo off
+
+rem Setup environment variables required for build.
+rem See INSTALL.txt for details.
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`stack path --compiler-bin`) DO (
+  SET CAIDE_GHC=%%F\..
+)
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir /AD /B "%CAIDE_GHC%\..\msys2*"`) DO (
+  SET CAIDE_MSYS=%CAIDE_GHC%\..\%%F\msys2_shell
+)
+
+SET PATH=%PATH%;%CAIDE_GHC%\mingw\bin;%CAIDE_GHC%\mingw\i686-w64-mingw32\bin
+
+rem Add additional PATH entries if necessary, e.g.:
+rem SET PATH=%PATH%;C:\cmake\bin;C:\Python27
+
