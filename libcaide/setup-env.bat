c9b94bd2589fb7664d05da51201793e5efe4f84c
slycelote <slycelot@yandex.ru>
2018-07-29 20:28:43 -0700

    Start MSYS shell in current directory

--- libcaide/setup-env.bat
+++ libcaide/setup-env.bat
@@ -8,7 +8,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`stack path --compiler-bin`) DO (
 )
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`dir /AD /B "%CAIDE_GHC%\..\msys2*"`) DO (
-  SET CAIDE_MSYS=%CAIDE_GHC%\..\%%F\msys2_shell
+  SET CAIDE_MSYS=%CAIDE_GHC%\..\%%F\msys2_shell -mingw32 -here
 )
 
 SET PATH=%PATH%;%CAIDE_GHC%\mingw\bin;%CAIDE_GHC%\mingw\i686-w64-mingw32\bin
