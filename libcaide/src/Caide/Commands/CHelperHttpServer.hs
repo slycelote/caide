144709c8c2f34b513d12c0613442061520623a8e
slycelote <slycelot@yandex.ru>
2018-08-01 00:40:46 -0700

    Support Competitive Companion browser extension
    Closes github issue #16.

--- libcaide/src/Caide/Commands/CHelperHttpServer.hs
+++ libcaide/src/Caide/Commands/CHelperHttpServer.hs
@@ -9,11 +9,18 @@ import Control.Applicative ((<$>), (<*>))
 import Control.Concurrent (forkIO, killThread)
 import Control.Monad (void)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
 import qualified Filesystem.Path as F
-import Network
+import qualified Filesystem.Path.CurrentOS as F
+import Network (accept, listenOn, sClose, withSocketsDo, PortID(PortNumber), Socket)
+import Network.Socket (tupleToHostAddress)
 import System.IO (hClose, hSetBuffering, stdout, stderr, BufferMode(NoBuffering))
 import System.IO.Error (tryIOError)
+
+import Data.Aeson (FromJSON(parseJSON), eitherDecode', withObject, (.:))
+import Network.Shed.Httpd (initServerBind, Request(..), Response(..))
 
 import Data.Text.Encoding.Util (tryDecodeUtf8, universalNewlineConversionOnInput)
 import qualified Data.Text.IO.Util as T
@@ -29,11 +36,71 @@ runHttpServer root = withSocketsDo $ do
     hSetBuffering stdout NoBuffering
     hSetBuffering stderr NoBuffering
     sock <- listenOn $ PortNumber 4243
-    T.putStrLn "Running HTTP server for CHelper extension. Press Return to terminate."
-    tid <- forkIO $ void . sequence . repeat $ processRequest sock root
+    T.putStrLn "Running HTTP server for CHelper and Competitive Companion extensions. Press Return to terminate."
+    chelperThreadId <- forkIO $ void . sequence . repeat $ processRequest sock root
+    companionThreadId <- forkIO $ initServerBind 8080 (tupleToHostAddress (127,0,0,1)) (processCompanionRequest root)
     _ <- getLine
-    killThread tid
+    killThread companionThreadId
+    killThread chelperThreadId
     sClose sock
+
+data ParsedProblem = Parsed Problem [TestCase]
+
+createProblem :: F.FilePath -> ParsedProblem -> IO ()
+createProblem root (Parsed problem testCases) = do
+   ret <- runInDirectory root $ do
+       saveProblem problem testCases
+       setActiveProblem $ problemId problem
+   case ret of
+       Left err -> putStrLn $ describeError err
+       _        -> return ()
+
+
+ok, badRequest :: Int
+ok = 200
+badRequest = 400
+
+makeResponse :: Int -> String -> Response
+makeResponse code message = Response code [] message
+
+
+instance FromJSON InputSource where
+  parseJSON = withObject "input" $ \o -> do
+    inputType <- o .: "type"
+    if inputType == ("file" :: String) then FileInput . F.fromText <$> o .: "fileName" else pure StdIn
+
+instance FromJSON OutputTarget where
+  parseJSON = withObject "output" $ \o -> do
+    outputType <- o .: "type"
+    if outputType == ("file" :: String) then FileOutput . F.fromText <$> o .: "fileName" else pure StdOut
+
+instance FromJSON TestCase where
+  parseJSON = withObject "test case" $ \o ->
+    TestCase <$> o .: "input" <*> o .: "output"
+
+instance FromJSON ParsedProblem where
+  parseJSON = withObject "problem description" $ \o -> do
+    input <- o .: "input"
+    output <- o .: "output"
+    probName <- o .: "name"
+    tests <- o .: "tests"
+    languages <- o .: "languages"
+    java <- languages .: "java"
+    probId <- java .: "taskClass"
+    return $ Parsed (Problem probName probId (Stream input output)) tests
+
+processCompanionRequest :: F.FilePath -> Request -> IO Response
+processCompanionRequest root request = do
+    let body = LBS.fromStrict . encodeUtf8 . T.pack $ reqBody request
+        mbParsed = eitherDecode' body
+    case mbParsed of
+        Left err -> do
+            putStrLn err
+            return $ makeResponse badRequest err
+        Right parsedProblem -> do
+            createProblem root parsedProblem
+            return $ makeResponse ok "OK"
+
 
 processRequest :: Socket -> F.FilePath -> IO ()
 processRequest sock root = void . tryIOError $ do
@@ -57,14 +124,6 @@ processRequest sock root = void . tryIOError $ do
 process :: T.Text -> T.Text -> F.FilePath -> IO ()
 process chid page root = case parseFromHtml <$> findHtmlParser chid <*> Just page of
     Nothing -> T.putStrLn . T.concat $ ["'", chid, "' not supported"]
-
     Just (Left err) -> T.putStrLn . T.concat $ ["Error while parsing the problem: ", err]
-
-    Just (Right (problem, testCases)) -> do
-        ret <- runInDirectory root $ do
-            saveProblem problem testCases
-            setActiveProblem $ problemId problem
-        case ret of
-            Left err -> putStrLn $ describeError err
-            _        -> return ()
+    Just (Right (problem, testCases)) -> createProblem root $ Parsed problem testCases
 
