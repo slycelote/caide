407adbd5cd15125cada127d14710ac0071ee88ba
slycelote <slycelot@yandex.ru>
2019-02-20 22:24:47 -0800

    Remove redundant environment variables

--- libcaide/setup-env.bat
+++ libcaide/setup-env.bat
@@ -9,6 +9,8 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`stack path --compiler-bin`) DO (
 
 SET PATH=%PATH%;%CAIDE_GHC%\mingw\bin;%CAIDE_GHC%\mingw\i686-w64-mingw32\bin
 
-rem Add additional PATH entries if necessary, e.g.:
+rem Change these additional PATH entries according to your system if necessary, e.g.:
 rem SET PATH=%PATH%;C:\cmake\bin;C:\Python27
+
+SET PATH=%PATH%;C:\bin\cmake\bin
 
--- libcaide/setup-test-env.sh
+++ libcaide/setup-test-env.sh
@@ -3,9 +3,6 @@
 # Setup environment variables required for running tests.
 # See INSTALL.txt for details.
 
-export CAIDE_GHC=$(echo /$CAIDE_GHC | sed 's/://g; s|\\|/|g')
-export PATH="$PATH:$CAIDE_GHC/mingw/bin:$CAIDE_GHC/mingw/i686-w64-mingw32/bin"
-
 # Change these paths according to your system.
 export CSC=/c/Windows/Microsoft.NET/Framework/v4.0.30319/csc.exe
 export PHANTOMJS=/c/bin/phantomjs.exe
