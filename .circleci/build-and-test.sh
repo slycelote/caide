23c4cc4c19ae11454434d85e226fe63273b69085
slycelote <slycelot@yandex.ru>
2019-02-24 13:08:35 -0800

    Update CircleCI config

--- .circleci/build-and-test.sh
+++ .circleci/build-and-test.sh
@@ -5,6 +5,10 @@ date
 
 sudo apt update
 sudo apt install g++ cmake cabal-install ghc binutils python2.7
+
+cmake --version
+g++ --version
+ghc --version
 date
 
 export CXX=g++
@@ -27,6 +31,7 @@ date
 
 export MONO=mono
 export CSC=mcs
+export QT_QPA_PLATFORM=offscreen
 tests/run-tests.sh
 date
 
