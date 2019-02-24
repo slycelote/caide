2f59ab10db984571aabead8657e9e35515c4c0e2
slycelote <slycelot@yandex.ru>
2019-02-24 11:39:30 -0800

    Add CircleCI config

---
+++ .circleci/build-and-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -ev
+
+date
+
+sudo apt install g++ cmake cabal-install ghc binutils
+date
+
+export CXX=g++
+
+git submodule update --init --recursive
+date
+
+cd libcaide
+cabal sandbox init
+cabal update -v
+cabal install --only-dependencies
+date
+
+cabal configure
+cabal build --ghc-options="-pgml $CXX"
+date
+
+sudo apt install phantomjs mono-mcs wget curl
+date
+
+export MONO=mono
+tests/run-tests.sh
+date
+
