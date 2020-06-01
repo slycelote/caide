11ed9ca16b68b1a1f37409d635df5ceb8eacee1c
slycelote <slycelot@yandex.ru>
2020-05-31 20:20:02 -0700

    Update setup-env.bat to work with plain ghc/cabal

--- libcaide/setup-env.bat
+++ libcaide/setup-env.bat
@@ -3,14 +3,17 @@
 rem Setup environment variables required for build.
 rem See INSTALL.txt for details.
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`stack path --compiler-bin`) DO (
-  SET CAIDE_GHC=%%F\..
+FOR /F "tokens=* USEBACKQ" %%F IN (`where ghc`) DO (
+  SET CAIDE_GHC=%%F\..\..
 )
 
-SET PATH=%PATH%;%CAIDE_GHC%\mingw\bin;%CAIDE_GHC%\mingw\i686-w64-mingw32\bin
+rem If you use Stack, use the following instead:
+rem FOR /F "tokens=* USEBACKQ" %%F IN (`stack path --compiler-bin`) DO (
+rem   SET CAIDE_GHC=%%F\..
+rem )
+
+SET PATH=%PATH%;%CAIDE_GHC%\mingw\bin;%CAIDE_GHC%\mingw\x86_64-w64-mingw32\bin;%CAIDE_GHC%\mingw\i686-w64-mingw32\bin
 
 rem Change these additional PATH entries according to your system if necessary, e.g.:
-rem SET PATH=%PATH%;C:\cmake\bin;C:\Python27
-
-SET PATH=%PATH%;C:\bin\cmake\bin
+SET PATH=%PATH%;C:\cmake\bin;C:\Python36
 
