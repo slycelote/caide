1fd8bc4b1d24443764953e7db55d1946eaaa7ed9
slycelote <slycelot@yandex.ru>
2020-08-23 00:42:03 -0700

    Fix an issue with a long test in Windows
    Ping command doesn't get killed on Ctrl+Break.

--- libcaide/tests/custom-lang/init/templates/lang1/build.bat
+++ libcaide/tests/custom-lang/init/templates/lang1/build.bat
@@ -1,2 +1,16 @@
-@ping 192.0.2.1 -n %CAIDE_TEST_BUILD_TIME% >nul
-@exit /b %CAIDE_TEST_BUILD_EXIT_CODE%
+@echo off
+:: The following is a way to sleep for CAIDE_TEST_BUILD_TIME seconds
+:: timeout command doesn't work with redirected stdin
+:: ping doesn't exit on ctrl-break so we're using a loop.
+setlocal
+set i=0
+
+:loop
+if %i% equ %CAIDE_TEST_BUILD_TIME% (goto :done)
+ping 192.0.2.1 -w 1000 -n 2 >nul
+set /a i+=1
+goto :loop
+
+:done
+exit /b %CAIDE_TEST_BUILD_EXIT_CODE%
+
--- libcaide/tests/custom-lang/init/templates/lang1/run.bat
+++ libcaide/tests/custom-lang/init/templates/lang1/run.bat
@@ -1,3 +1,16 @@
-@ping 192.0.2.1 -n %CAIDE_TEST_RUN_TIME% >nul
-@echo 1 2 3
-@exit /b %CAIDE_TEST_RUN_EXIT_CODE%
+@echo off
+
+
+setlocal
+set i=0
+
+:loop
+if %i% equ %CAIDE_TEST_RUN_TIME% (goto :done)
+ping 192.0.2.1 -w 1000 -n 2 >nul
+set /a i+=1
+goto :loop
+
+:done
+echo 1 2 3
+exit /b %CAIDE_TEST_RUN_EXIT_CODE%
+
