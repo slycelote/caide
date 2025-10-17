ddb80cc66d81128121cb504efc921e703b6b32e1
slycelote <slycelot@yandex.ru>
2025-10-17 16:59:49 -0700

    Remove old docs

--- libcaide/doc/dev/universal-executable-build/conf/env32.sh
+++
@@ -1,6 +0,0 @@
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
-export CABAL_CONFIG=~/conf/cabalconfig32
-export PATH=~/bin32:~/local32/bin:"$PATH":~/bin
-export GHC_LINKER_32=1
-export CAIDE_CMAKE_CONFIGURE_ARGS="-G,Ninja"
-
